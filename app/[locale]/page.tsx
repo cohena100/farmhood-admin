0dcc796bee0bd7a88f581a8028eefbac10f6fbdd
Avi Cohen <cohena100@gmail.com>
2024-03-13 21:41:35 +0200

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -84,42 +84,46 @@ GROUP BY p.id`;
           className="my-4 font-bold text-lg  text-pink-600 dark:text-pink-500 "
         />
       )}
-      {unusualOrders.map((order) => (
-        <Card key={order.id} className="max-w-fit">
-          <Avatar img={order.imageUrl ?? ""} className="max-w-fit" rounded>
-            <div className="ms-2 space-y-1 font-medium dark:text-white">
-              <div>{order.name}</div>
-              <div className="text-sm text-gray-500 dark:text-gray-400">
-                {order.phone}
+      <div className="grid gap-2 grid-cols-1 sm:grid-cols-2">
+        {unusualOrders.map((order) => (
+          <Card key={order.id}>
+            <Avatar img={order.imageUrl ?? ""} className="max-w-fit" rounded>
+              <div className="ms-2 space-y-1 font-medium dark:text-white">
+                <div>{order.name}</div>
+                <div className="text-sm text-gray-500 dark:text-gray-400">
+                  {order.phone}
+                </div>
+                <div className="text-sm text-gray-500 dark:text-gray-400">
+                  {t(order.parkingLot.name)}
+                </div>
               </div>
-              <div className="text-sm text-gray-500 dark:text-gray-400">
-                {t(order.parkingLot.name)}
-              </div>
-            </div>
-          </Avatar>
-          <Table>
-            <TableBody className="divide-y text-start">
-              {order.products.map((product) => (
-                <TableRow key={product.productId}>
-                  <TableCell>{t(product.product.title)}</TableCell>
-                  <TableCell className="text-center">
-                    {product.quantity > 4 ? (
-                      <Badge color="pink">{product.quantity}</Badge>
-                    ) : (
-                      product.quantity
-                    )}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <ActionButton
-            className="self-start mb-4"
-            label={t("Sell")}
-            id={order.id}
-          />
-        </Card>
-      ))}
+            </Avatar>
+            <Table>
+              <TableBody className="divide-y">
+                {order.products.map((product) => (
+                  <TableRow key={product.productId}>
+                    <TableCell className=" text-start">
+                      {t(product.product.title)}
+                    </TableCell>
+                    <TableCell className="flex justify-center items-center">
+                      {product.quantity > 4 ? (
+                        <Badge color="pink">{product.quantity}</Badge>
+                      ) : (
+                        product.quantity
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <ActionButton
+              className="self-start mt-auto"
+              label={t("Sell")}
+              id={order.id}
+            />
+          </Card>
+        ))}
+      </div>
       <Link
         href="/orders"
         className="font-medium text-pink-600 dark:text-pink-500 underline my-4"
